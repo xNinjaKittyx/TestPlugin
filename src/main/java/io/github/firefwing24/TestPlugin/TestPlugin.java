003bdcce1b622632735b49a1f37e0ffe938761e8
Daniel Ahn <sangbumahn@gmail.com>
2014-08-06 23:08:30 -0700

    v. 0.0.2 - added /kill, /sethome, /home, /w, /who

--- src/main/java/io/github/firefwing24/TestPlugin/TestPlugin.java
+++ src/main/java/io/github/firefwing24/TestPlugin/TestPlugin.java
@@ -6,6 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin {
 
+	public static TestPlugin plugin;
+	
 	public TestPlugin() {
 		// TODO Auto-generated constructor stub
 	}
@@ -15,9 +17,14 @@ public final class TestPlugin extends JavaPlugin {
 	}
 	@Override
 	public void onEnable(){
-		
+
+		this.getCommand("home").setExecutor(new TestPluginCommandExecutor (this));
+		this.getCommand("kill").setExecutor(new TestPluginCommandExecutor (this));
+		this.getCommand("sethome").setExecutor(new TestPluginCommandExecutor (this));
 		this.getCommand("test").setExecutor(new TestPluginCommandExecutor (this));
 		this.getCommand("tp").setExecutor(new TestPluginCommandExecutor (this));
+		this.getCommand("who").setExecutor(new TestPluginCommandExecutor (this));
+		this.getCommand("w").setExecutor(new TestPluginCommandExecutor(this));
 		
 		PluginDescriptionFile pdfFile = this.getDescription();
 		getLogger().info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
--- src/main/java/io/github/firefwing24/TestPlugin/TestPluginCommandExecutor.java
+++ src/main/java/io/github/firefwing24/TestPlugin/TestPluginCommandExecutor.java
@@ -1,5 +1,10 @@
 package io.github.firefwing24.TestPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,6 +13,7 @@ import org.bukkit.entity.Player;
 
 public class TestPluginCommandExecutor implements CommandExecutor {
 	private final TestPlugin plugin;
+	ArrayList <String> onlinePlayers = new ArrayList <String>();
  
 	public TestPluginCommandExecutor(TestPlugin plugin) {
 		this.plugin = plugin; // Store the plugin in situations where you need it.
@@ -15,16 +21,18 @@ public class TestPluginCommandExecutor implements CommandExecutor {
  
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		//TEST COMMAND
 		if (cmd.getName().equalsIgnoreCase("test")){
 			
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
 				if (player.hasPermission("TestPlugin.test") || player.isOp()) {
-					player.sendMessage("TestPlugin is working! Sent from Player");
+					player.sendMessage(ChatColor.GREEN + "TestPlugin is working! Sent from Player");
 					return true;
 				}
 				else {
-					player.sendMessage("You don't have permission!");
+					player.sendMessage(ChatColor.RED + "You don't have permission!");
 					return true;
 				}
 			} else {
@@ -34,21 +42,26 @@ public class TestPluginCommandExecutor implements CommandExecutor {
 			
 		}
 		
+		//TP COMMAND
 		if (cmd.getName().equalsIgnoreCase("tp")) { //example tp command
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
 				if ((args.length < 1) || (args.length > 3)) {
-					sender.sendMessage("Invalid Arguments!");
+					sender.sendMessage(ChatColor.RED + "Invalid Arguments!");
 					return false;
 				}
 				if (args.length == 1) {
 					if (player.hasPermission("TestPlugin.tp.me")) {
 						Player targetPlayer = player.getServer().getPlayer(args[0]);
-						Location location = targetPlayer.getLocation();
-						player.teleport(location);
+						if (targetPlayer.isOnline()) {
+							Location location = targetPlayer.getLocation();
+							player.teleport(location);
+						} else {
+							player.sendMessage(ChatColor.RED + args[0] + " is not Online!");
+						}
 					}
 					else
-						sender.sendMessage("You don't have permission!");
+						sender.sendMessage(ChatColor.RED + "You don't have permission!");
 					//TP directly to argument player.
 					return true;
 				}
@@ -56,13 +69,21 @@ public class TestPluginCommandExecutor implements CommandExecutor {
 					if (player.hasPermission("TestPlugin.tp.others")) {
 						Player target1 = player.getServer().getPlayer(args[0]);
 						Player target2 = player.getServer().getPlayer(args[1]);
-						
-						Location location = target2.getLocation();
-						
-						target1.teleport(location);
+						if (target1.isOnline() && target2.isOnline()) {
+							Location location = target2.getLocation();
+							target1.teleport(location);
+							target1.sendMessage(ChatColor.YELLOW + "You have been teleported to " + target2.getDisplayName());
+						} else {
+							if (!target1.isOnline()) {
+								player.sendMessage(ChatColor.RED + args[0] + "is not Online!");
+							}
+							if (!target2.isOnline()) {
+								player.sendMessage(ChatColor.RED + args[1] + "is not Online!");
+							}
+						}
 					}
 					else
-						sender.sendMessage("You don't have permission!");
+						sender.sendMessage(ChatColor.RED + "You don't have permission!");
 					//TP from arg1 to arg2
 					return true;
 				}
@@ -77,12 +98,12 @@ public class TestPluginCommandExecutor implements CommandExecutor {
 							player.teleport(location);
 							//TP to coordinates (arg1,arg2,arg3)
 						}	catch (NumberFormatException ex) {
-							player.sendMessage("Invalid Location");
+							player.sendMessage(ChatColor.RED + "Invalid Location");
 						}
 						return true;
 					}
 					else
-						sender.sendMessage("You don't have permission!");
+						sender.sendMessage(ChatColor.RED + "You don't have permission!");
 					return true;
 				}
 				else
@@ -92,12 +113,21 @@ public class TestPluginCommandExecutor implements CommandExecutor {
 			
 			else {
 				if (args.length == 2) {
+					
 					Player target1 = sender.getServer().getPlayer(args[0]);
 					Player target2 = sender.getServer().getPlayer(args[1]);
-					
-					Location location = target2.getLocation();
-					
-					target1.teleport(location);
+					if (target1.isOnline() && target2.isOnline()) {
+						Location location = target2.getLocation();
+						target1.teleport(location);
+						target1.sendMessage(ChatColor.YELLOW + "You have been teleported to " + target2.getDisplayName());
+					} else {
+						if (!target1.isOnline()) {
+							sender.sendMessage(ChatColor.RED + args[0] + "is not Online!");
+						}
+						if (!target2.isOnline()) {
+							sender.sendMessage(ChatColor.RED + args[1] + "is not Online!");
+						}
+					}
 					//TP arg1 to arg2
 				}
 				else {
@@ -107,6 +137,111 @@ public class TestPluginCommandExecutor implements CommandExecutor {
 			}
 			return false;
 		}
+		
+		//WHO COMMAND
+		if (cmd.getName().equalsIgnoreCase("who")) {
+			if (sender instanceof Player)
+			{
+				Player player = (Player) sender;
+				if (player.hasPermission("TestPlugin.who"))
+				{
+					for (Player p : Bukkit.getOnlinePlayers()) {
+						onlinePlayers.add(p.getName());
+					}
+					sender.sendMessage(ChatColor.YELLOW + "Online Players: " + ChatColor.WHITE + onlinePlayers.toString());
+				
+				}
+			}
+		}
+		
+		//Whisper Command
+		
+		if (cmd.getName().equalsIgnoreCase("w")) {
+			if (!sender.hasPermission("TestPlugin.whisper")) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission!");
+			}
+			
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This can only be used by in-game Players");
+				return true;
+			}
+			
+
+			Player player = (Player) sender;
+			if (args.length < 2) {
+				return false;
+			}
+			
+			Player target = player.getServer().getPlayer(args[0]);
+			if (!target.isOnline()) {
+				player.sendMessage(ChatColor.RED + args[0] + " is not online!");
+				return true;
+			} else {
+				
+				target.sendMessage(player.getName() + ": ");
+				ArrayList <String> message = new ArrayList <String>();
+				for (int i=1; i<args.length;i++)
+				{
+					message.add(args[i]);
+				}
+				
+				target.sendMessage(sender.getName() + ": " + message.toString());
+				return true;
+			}
+			
+		}
+		
+		if (cmd.getName().equalsIgnoreCase("kill")) {
+			if (!sender.hasPermission("TestPlugin.kill")) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission!");
+			}
+			Player target = sender.getServer().getPlayer(args[0]);
+			
+			if (args.length < 1)
+			
+			if (target == null) {
+				sender.sendMessage(ChatColor.RED + args[0] + "is not online");
+			} else {
+				
+				target.setHealth(0);
+			}
+			return true;
+		}
+		
+		if (cmd.getName().equalsIgnoreCase("sethome")) {
+			if (!sender.hasPermission("TestPlugin.sethome")) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission!");
+			}
+			
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This can only be used by in-game Players");
+				return true;	
+			}
+			
+			Player player = (Player) sender;
+			Location location = player.getLocation();
+			player.setBedSpawnLocation(location);
+			player.sendMessage(ChatColor.GREEN + "Home Set!");
+		}
+		
+		if (cmd.getName().equalsIgnoreCase("home")) {
+			if (!sender.hasPermission("TestPlugin.home")) {
+				sender.sendMessage(ChatColor.RED + "You don't have permission!");
+			}
+			
+			
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This can only be used by in-game Players");
+				return true;	
+			}
+			
+			Player player = (Player) sender;
+			Location location = player.getBedSpawnLocation();
+			player.teleport(location);
+			player.sendMessage(ChatColor.GREEN + "Teleported to Home");
+		}
+		
+		
 		
 		//args.length is number of arguments. args is an array :)
 		/*
